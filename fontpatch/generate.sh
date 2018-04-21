074828d70db8d4c66f7f66391c62f02ac5fc890c
seiyeah78 <ketcoding@gmail.com>
2018-04-22 01:21:40 +0900

    update font generator

--- fontpatch/generate.sh
+++ fontpatch/generate.sh
@@ -3,15 +3,25 @@
 
 arg=$1
 font_name=${arg:-'RictyDiminishedDiscord-Regular'}
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
 
-cd nerd-fonts
+if [ ! -d ./nerd-fonts ]; then
+  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
+fi
 
 if [ type fontforge > /dev/null 2>&1 ]; then
   brew install fontforge
 fi
 
-fontforge -script ./font-patcher ~/Library/Fonts/$font_name.ttf -c -l
+# see : https://github.com/ryanoasis/nerd-fonts/issues/225
+pyenv install
+python -c "import configparser"
+if [ $? != 0 ]; then
+  pip install configparser
+  mkdir -p ~/Library/Python/2.7/lib/python/site-packages
+  echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
+fi
+
+fontforge -script ./nerd-fonts/font-patcher ~/Library/Fonts/$font_name.ttf -c -l
 
 open .
 
