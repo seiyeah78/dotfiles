fd0d105a968aff99bd330ad1d76683b33d3cd4aa
se-ya <ketcoding@gmail.com>
2023-11-19 03:52:05 +0900

    update

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  'gkeep/iceberg-dark',
+  'joshdick/onedark.vim',
+  'rhysd/vim-color-spring-night',
+  'arcticicestudio/nord-vim',
+  'romainl/Apprentice',
+  'mhartington/oceanic-next',
+  {
+    'haishanh/night-owl.vim',
+    config = function()
+      vim.cmd([[colorscheme night-owl]])
+    end
+  },
+  'sainnhe/sonokai',
+}
