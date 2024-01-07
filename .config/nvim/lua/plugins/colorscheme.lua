2da31ef8ece34596a504d32055d9d3f67663fa2a
se-ya <ketcoding@gmail.com>
2024-01-07 15:24:39 +0900

    add vim split border

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,10 @@ return {
     opts = {},
     config = function()
       require('tokyonight').setup({
-        style = "moon"
+        style = "moon",
+        on_colors = function(colors)
+          colors.border = "#565f89"
+        end
       })
       vim.cmd [[colorscheme tokyonight]]
     end
