8f333fde60d2605d9e6a1a070777cbba589cb0b8
se-ya [Copilot] <ketcoding@gmail.com>
2024-02-01 12:17:45 +0900

    update

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -24,14 +24,27 @@ return {
       { 'hrsh7th/cmp-nvim-lsp' },
     },
     config = function()
-      require('mason-lspconfig').setup_handlers({ function(server)
-        local opt = {
-          capabilities = require('cmp_nvim_lsp').default_capabilities(
-            vim.lsp.protocol.make_client_capabilities()
-          )
-        }
-        require('lspconfig')[server].setup(opt)
-      end })
+      require('mason-lspconfig').setup_handlers({
+        function(server)
+          local opt = {
+            capabilities = require('cmp_nvim_lsp').default_capabilities(
+              vim.lsp.protocol.make_client_capabilities()
+            )
+          }
+          require('lspconfig')[server].setup(opt)
+          require("lspconfig").pyright.setup {
+            settings = {
+              python = {
+                venvPath = ".",
+                pythonPath = "./.venv/bin/python",
+                analysis = {
+                  extraPaths = { "." }
+                }
+              }
+            }
+          }
+        end
+      })
     end
   },
   {
