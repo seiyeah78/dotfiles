41029587b5c48674a620827577eac0b7f310009a
se-ya [Copilot] <ketcoding@gmail.com>
2024-05-15 19:10:10 +0900

    add format on save

--- .config/nvim/lua/autocmds/init.lua
+++ .config/nvim/lua/autocmds/init.lua
@@ -1,1 +1,15 @@
 vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format() end, {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        if vim.api.nvim_buf_get_option(args.buf, 'filetype') == "gitcommit" then
+          return
+        end
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -86,26 +86,6 @@ return {
         },
         handlers = {},
       })
-
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      require("null-ls").setup({
-        -- you can reuse a shared lspconfig on_attach callback here
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                -- if vim.api.nvim_buf_get_option(bufnr, 'filetype') == "gitcommit" then
-                --   return
-                -- end
-                vim.lspbuf.format({ async = false })
-              end,
-            })
-          end
-        end,
-      })
     end,
   },
   {
