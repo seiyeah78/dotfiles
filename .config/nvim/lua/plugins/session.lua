4376c2aa7272d4de0b66c661a548394bf6accf29
se-ya <ketcoding@gmail.com>
2025-10-17 10:37:51 +0900

    update

--- .config/nvim/lua/plugins/session.lua
+++ .config/nvim/lua/plugins/session.lua
@@ -4,9 +4,10 @@ return {
     lazy = false,
     opts = {
       suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/", "~/projects", "~/codes" },
+      close_filetypes_on_save = { 'checkhealth', 'qf' }
     },
-    config = function()
-      require('auto-session').setup()
+    config = function(_, opts)
+      require('auto-session').setup(opts)
       vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
     end,
   },
